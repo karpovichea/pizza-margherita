feb8f4c40af81fd7d0d401b698033c7bfa071f85
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-11-04 17:01:57 +0300

    refactor class MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -4,19 +4,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPage {
-    private WebDriver driver;
+    private WebDriverWait wait;
 
     @FindBy(xpath = "//a[@href='https://www.pizzatempo.by/menu/pizza.html']")
-    WebElement pizzaButton;
+    WebElement pizzaPageButton;
+
+    @FindBy(xpath = "//a[@href='https://www.pizzatempo.by/menu/order/']")
+    WebElement basketPageButton;
 
     public MainPage(WebDriver driver) {
-        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         PageFactory.initElements(driver, this);
     }
 
     public void clickPizzaButton() {
-        pizzaButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(pizzaPageButton));
+        pizzaPageButton.click();
+    }
+
+    public void clickBasketPageButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(basketPageButton));
+        basketPageButton.click();
     }
 }
